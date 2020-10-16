4a2dee9a530f767f69ce45c58cdf64e27c85518b
BrandonHorvatic <60112352+BrandonHorvatic@users.noreply.github.com>
2020-10-17 01:07:28 +0200

    Initial commit

---
+++ src/app/components/products-route/products-route.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-route',
+  templateUrl: './products-route.component.html',
+  styleUrls: ['./products-route.component.scss']
+})
+export class ProductsRouteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/profit-science-liquid/profit-science-liquid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profit-science-liquid',
+  templateUrl: './profit-science-liquid.component.html',
+  styleUrls: ['./profit-science-liquid.component.scss']
+})
+export class ProfitScienceLiquidComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
